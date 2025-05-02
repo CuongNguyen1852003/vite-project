ba6bca0c31c94773768587aa3a9c95b82fd56ef2
CuongNguyen1852003 <nttc1852003@gmail.com>
2025-05-02 22:18:10 +0700

    DOM EVENTS

--- vite-project/src/components/todo/ToDoNew.jsx
+++ vite-project/src/components/todo/ToDoNew.jsx
@@ -1,8 +1,14 @@
 const ToDoNew = () => {
+    const handleClick = () => {
+        alert("Click me!")
+    }
+    const handleOnChange = (name) => {
+        console.log(">>> handleOnChange", name) // event.target trở tới input, value giá trị của ô input
+    } // Mỗi 1 lần gõ vào là onChange đc kích hoạt
     return (
         <div className="todo-new">
-            <input type="text" />
-            <button>Add</button>
+            <input type="text" onChange={(event) => handleOnChange(event.target.value)} /> 
+            <button style={{cursor: "pointer"}} onClick={handleClick}>Add</button>
         </div>
     )
 }
