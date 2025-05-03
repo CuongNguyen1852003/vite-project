0ecc0aa99c1673ef3d19310aa7aa6cce5abf5792
CuongNguyen1852003 <nttc1852003@gmail.com>
2025-05-03 15:12:36 +0700

    updated

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,18 +2,35 @@ import "./components/todo/todo.css";
 import ToDoNew from "./components/todo/ToDoNew";
 import ToDoData from "./components/todo/ToDoData";
 import reactLogo from "./assets/react.svg";
+import { useState } from "react";
 
 const App = () => {
-  return (
+    const [todoList, setTodoList] = useState([
+      {id: 1, name: "Learning React"},
+      {id: 2, name: "Watching Youtube"}
+    ])
+
+    const addNewTodo = (name) => {
+      const newTodo = {
+        id: 3,
+        name: name
+      }
+      setTodoList([...todoList, newTodo])
+    }
+    return(
     <div className="todo-container">
       <div className="todo-title">Todo List </div>
-      <ToDoNew />
-      <ToDoData />
+      <ToDoNew 
+        addNewTodo = {addNewTodo}
+      />
+      <ToDoData 
+        todoList = {todoList}
+      />
       <div className="todo-image">
         <img src={reactLogo} alt="React Logo" className="logo" />
       </div>
     </div>
-  );
+    );
 };
 
 export default App;
